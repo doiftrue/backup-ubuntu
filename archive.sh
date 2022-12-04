5fb322bb386b3ca7c63988f175d32ea79bbd62c5
Kama <tkama@bk.ru>
2022-12-04 21:02:56 +0500

    archive.sh improvements

--- archive.sh
+++ archive.sh
@@ -5,4 +5,8 @@ THE_FILE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 source "$THE_FILE_DIR"/_vars.sh
 source "$THE_FILE_DIR"/_mount.sh
 
-sudo tar cf "$BACKUP_MOUNT_DIR/$BACKUP_FOLDER_NAME-$(date +%Y-%m-%d)".tar "$DESTINATION_DIR"
+cd "$BACKUP_MOUNT_DIR/$BACKUP_FOLDER_NAME" || exit
+
+TAR_FNAME="$BACKUP_FOLDER_NAME-$(date +%Y-%m-%d).tar"
+
+sudo tar --exclude="home/kama/Music/*" -cf "$BACKUP_MOUNT_DIR/$TAR_FNAME" -- *
